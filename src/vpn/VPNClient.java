8c4d4329cf065d3761f7630b6ba24fbdd516aa54
Muhammad Qasim Ali Tareen <qk04504@gmail.com>
2025-04-27 22:07:10 +0500

    Initial commit

--- src/vpn/VPNClient.java
+++ src/vpn/VPNClient.java
@@ -1,20 +1,59 @@
 package vpn;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class VPNClient {
-    private String server_address;
-
-    public VPNClient(String server_address)
-    {
-        this.server_address=server_address;
+    public static void main(String[] args) {
+        VPNClient client1 = new VPNClient();
+        client1.G_u_I();
     }
 
-    public void connect()
+    public void G_u_I()
     {
-        System.out.println("Connecting to the server at : "+server_address);
-        // Placeholder for the real server logical connection
+        JFrame frame = new JFrame("VPN CLIENT ");
+        frame.setSize(300,200);
+        frame.setLayout(new FlowLayout());
+
+        JLabel label = new JLabel("Enter the VPN Server ID : ");
+        JTextField serverField = new JTextField(20);
+        JButton connectButton = new JButton("Connect");
+        JLabel statusLabel = new JLabel("Status : Not Connected :( ");
+
+        connectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String server_IP = serverField.getText();
+                if(!server_IP.isEmpty())
+                {
+                    statusLabel.setText("Status: Connected to "+server_IP);
+                    VPNLogManager.log("Connected to server : "+server_IP);
+                }
+                else
+                {
+                    statusLabel.setText("status : No IP Detected/Entered ");
+                    VPNLogManager.log("No IP Entered ");
+                }
+            }
+        });
+
+        frame.add(label);
+        frame.add(serverField);
+        frame.add(connectButton);
+        frame.add(statusLabel);
+
+        frame.setVisible(true);
     }
-    public void disconnect()
-    {
-        System.out.println("Disconnected from vpn :( ");
+
+    public void connect() {
+        System.out.println(" Connecting to VPN... ");
+        VPNLogManager.log("Connecting to VPN .... ");
+    }
+
+    public void disconnect() {
+        System.out.println("Disconnecting from the VPN .... ");
+        VPNLogManager.log("Disconnecting from VPN ... ");
     }
 }
